0f908dd65e01dfc974df4784ffd0104e272af4d9
notobd <notobd4x4@gmail.com>
2024-04-10 13:56:37 -0700

    struggling

---
+++ index.js
@@ -0,0 +1,15 @@
+{
+  document.getElementById("demo").innerHTML = "I must learn javascript";
+}
+{
+  let x, y, z;
+  x = 6;
+  y = 6;
+  z = x + y;
+  document.getElementById("demo1").innerHTML = z;
+}
+const carName = "Volvo";
+window.alert(carName);
+{
+  alert(10 * 5);
+}
